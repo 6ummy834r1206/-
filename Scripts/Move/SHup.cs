1cd3476d4882734f285e7e0b0a31820508490f4f
6ummy834r1206 <73460497+6ummy834r1206@users.noreply.github.com>
2022-10-03 22:22:28 +0800

    Add files via upload

---
+++ Scripts/Move/SHup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHup : MonoBehaviour {
+    public GameObject Target;
+    public float Posy;
+    public float Posx;
+    public float Posz;
+    // Use this for initialization
+    void Start () {
+        Posx = Target.GetComponent<Transform>().position.x;
+        Posy = Target.GetComponent<Transform>().position.y;
+        Posz = Target.GetComponent<Transform>().position.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Target.GetComponent<Transform>().position = new Vector3(Posx, Mathf.Lerp(Posy-9f, Posy+12, Mathf.PingPong(Time.time*0.3f, 1)), Posz);
+    }
+}
